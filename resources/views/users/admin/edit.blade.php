82f3fec2dd441ee3edc5aea631939fbe64fabf75
Paúl Cuenca <pacuenca@utpl.edu.ec>
2018-06-26 19:37:55 -0500

    cambios edit

---
+++ resources/views/users/admin/edit.blade.php
@@ -0,0 +1,58 @@
+
+@extends('layouts.app')
+
+@section('content')
+<div class="box_aux"></div>
+	<div class="container">
+		<h1>Edición de Perfil: </h1>
+
+		<form method="PATCH" action=['UserController@update', {{Auth::user()->id}}]>
+		  		<div class="form-group">
+				    <label for="nombre">Nombre:</label>
+				    <input type="text" class="name" id="name" name="nombre" value="{{ Auth::user()->nombres }}">
+			 	</div>
+
+			 	<div class="form-group">
+				    <label for="nombre">Apellidos:</label>
+				    <input type="text" class="name" id="name" name="apellido" value="{{ Auth::user()->apellidos }}">
+			 	</div>
+
+			 	<div class="form-group">
+				    <label for="nombre">Correo:</label>
+				    <input type="text" class="name" id="name" name="correo" value="{{ Auth::user()->correo }}">
+			 	</div>
+
+			 	<div class="form-group">
+				    <label for="nombre">Telefono:</label>
+				    <input type="text" class="name" id="name" name="telefono" value="{{ Auth::user()->telefono }}">
+			 	</div>
+
+			 	<div class="form-group">
+				    <label for="nombre">Departamento:</label>
+				    <input type="text" class="name" id="name" name="departamento" value="{{ Auth::user()->departamento }}">
+			 	</div>
+
+			 	<div class="form-group">
+				    <label for="nombre">Sección:</label>
+				    <input type="text" class="name" id="name" name="seccion" value="{{ Auth::user()->seccion }}">
+			 	</div>
+		  		
+		  		<div class="form-group">
+				    <label for="nombre">Username:</label>
+				    <input type="text" class="name" id="name" name="username" value="{{ Auth::user()->username }}">
+			 	</div>
+
+			 	<div class="form-group">
+				    <label for="nombre">Password:</label>
+				    <input type="text" class="name" id="name" name="password" value="{{ Auth::user()->password }}">
+			 	</div>
+
+			 	<div class="form-group">
+				    <label for="nombre">Tipo de usuario:</label>
+				    <input type="text" class="name" id="name" name="tipo" value="{{ Auth::user()->tipousuario }}">
+			 	</div>
+		<button type="submit" class="btn btn-success">Actualizar</button>
+	</form>	
+</div>
+
+@endsection
