1b690065da57e2443148b968b2b51b0bf523c9ea
lcpacheco1 <lgcp.mf.9@gmail.com>
2018-07-17 12:31:50 -0500

    modificando plantilla2

--- resources/views/plantillas/plantilla2.blade.php
+++ resources/views/plantillas/plantilla2.blade.php
@@ -1,51 +1,42 @@
-@extends('layouts.app')
+@extends('layouts.app') @section('content')
 
-@section('content')
+<script>
+$(document).ready(function(){
+    $("btn_editar").click(function(){
+        $(this).fadeOut();
+    });
+});
+</script>
 <div class="box_aux"></div>
 
 <div class="container" id="cont_plan1">
-	<div class="row">
-		<div class="col-md-2">
-				@include('layouts.controlInfografia')
-				
-		</div>
-	
-		<div class="col-md-10 plantilla1" id="plantilla1">
-		<div class = "text-right">
-			<button type="button" class="btn btn-success ">Editar</button>
-			<button type="button" class="btn btn-success">Guardar</button>
-			<button type="button" class="btn btn-success">Enviar </button>
-		</div>
-		<ul id="myMenu">
-			<li><a href="#">HTML</a></li>
-			<li><a href="#">CSS</a></li>
-			<li><a href="#">JavaScript</a></li>
-			<li><a href="#">PHP</a></li>
-			<li><a href="#">Python</a></li>
-			<li><a href="#">jQuery</a></li>
-			<li><a href="#">SQL</a></li>
-			<li><a href="#">Bootstrap</a></li>
-			<li><a href="#">Node.js</a></li>
-		</ul>	
-				
-   		</div>
-				</div>
-				
-		</div>
-	</div>
-
-</div>
-
-<div class="container" id="cont_plan2">
     <div class="row">
         <div class="col-md-2">
-                @include('layouts.controlInfografia')
+            @include('layouts.controlInfografia')
+
         </div>
 
-        <div class="col-md-10 plantilla2" id="plantilla2">
-            hola
+        <div class="col-md-8 plantilla1" id="plantilla1">
+            <div class="text-right">
+                <button type="button" class="btn btn-success " id = "btn_editar">Editar</button>
+                <button type="button" class="btn btn-success">Guardar</button>
+                <button type="button" class="btn btn-success">Enviar </button>
+            </div class = "col-md-4">
+				<element  id="arrastrar" style="left:0;top:0;" contenteditable="false">Hola </element>
+			<div >
+			</div>
         </div>
     </div>
+</div>
+<script>
+/*
+		$(document).ready(function()
+{
+        $("btn_editar").click(function () {    
+	    alert("Bien!!!, la edad seleccionada es: "	);
+	    //alert("Bien!!!, la edad seleccionada es: " + $(this).val());  
+         });
+});*/
 
-
+</script>
 @endsection
